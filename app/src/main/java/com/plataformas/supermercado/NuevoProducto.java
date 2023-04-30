7074b16d0c6bf0ea7645241a900482a2505d5128
Adriana Valencia <adri_mvc@yahoo.com>
2023-04-29 21:33:47 -0500

    Nuevas actividades vacías, nuevo producto y modificar producto

---
+++ app/src/main/java/com/plataformas/supermercado/NuevoProducto.java
@@ -0,0 +1,14 @@
+package com.plataformas.supermercado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevoProducto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_producto);
+    }
+}
--- app/src/main/java/com/plataformas/supermercado/ProductosActivity.java
+++ app/src/main/java/com/plataformas/supermercado/ProductosActivity.java
@@ -2,13 +2,29 @@ package com.plataformas.supermercado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ProductosActivity extends AppCompatActivity {
+
+    ImageButton botonNuevoProd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
+
+        botonNuevoProd = findViewById(R.id.btnNuevoProd);
+
+        botonNuevoProd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), NuevoProducto.class));
+            }
+        });
     }
 }
